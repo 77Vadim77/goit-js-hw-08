3034582eab68ae551308b89aef83258a32d45ff9
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-08-26 15:21:49 +0300

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import "Simplelightbox/dist/Simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+function renderGaleery() {
+    const createEl = galleryItems
+        .map(
+            (item) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+        </a>
+        </li>`
+        )
+        .join("");
+    gallery.innerHTML = createEl;
+}
+
+renderGaleery();
+
+let lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+console.log(lightbox)
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('[name="email"]')
+const textarea = document.querySelector('[name="message"]')
+const LOCALST_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onInput, 500));
+    function onInput(event) {
+    const data = JSON.parse(localStorage.getItem('feedback-form-state')) || {}
+    data[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(data))
+        console.log("data on input" + data)
+        
+form.addEventListener('submit', onSubmit);
+function onSubmit() {
+    if (!email.value || !textarea.value) {
+        alert ("Будь ласка, заповніть усі поля")
+    }
+    else {
+        console.log(`email: ${email.value} massage: ${textarea.value}`)
+        localStorage.removeItem('feedback-form-state');
+        reset(form);
+    }
+};
+        window.addEventListener('load', onLoad);
+        function onLoad() {
+            const dataOn = JSON.parse(localStorage.getItem('feedback-form-state'))
+            console.log(dataOn)
+            if (dataOn) {
+                textarea.value = dataOn.message ? dataOn.message : '';
+                email.value = dataOn.email
+            }
+        }
