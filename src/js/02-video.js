3cc52ef9434e4688c1f9b4bf9b4ef02e463671ee
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-09-26 14:45:30 +0300

    5467

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector(`iframe`);
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const CURRENT_TIME_KEY = `video-player-timekey`;
-function getCurrentTimeKey(timeKey){
-    localStorage.setItem(CURRENT_TIME_KEY, timeKey.second);
-}
-player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
-
-const currentTimeValue = localStorage.getItem(CURRENT_TIME_KEY) || 0;
-player.setCurrentTime(currentTimeValue);
+player.on('timeupdate',
+    throttle(function ({ seconds }) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+ }, 1000)
+);
 
