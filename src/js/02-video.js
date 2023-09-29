274e013c0e8587c109236c0f525e08c3dd56350f
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-09-30 01:57:44 +0300

    update 320193

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,24 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new Player(iframe, {
+    loop: true,
+    fullscreen: true,
+    quality: '1080p',
+});
 
-player.on('timeupdate',
-    throttle(function ({ seconds }) {
-        localStorage.setItem('videoplayer-current-time', seconds);
- }, 1000)
-);
+const getCurrentTime = function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
 
+player.on('timeupdate', throttle(getCurrentTime, 1000)); 
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+player
+    .setColor('#d8e0ff')
+    .then(function (color) {
+        console.log('The new color value: #D8E0FF');
+    });
